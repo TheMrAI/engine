b024b3b8af6f2a877568c2a68cc6cc589adf4bfe
Istvan Davorin Acs <mr.istvan.acs@gmail.com>
2025-07-21 21:26:06 +0200

    Extend mul trait with reference based variants
    The goal is to support all these operations:
    Vector<T> * T
    Vector<T> * &T
    &Vector<T> * T
    &Vector<T> * &T
    The rest of the possible compinations produced by variants of T
    itself should be handled by the regular template substitution.
    
    Why the last 3 options?
    A library should impose the least amount of limitations and it should
    not be necessary to force the user on cloniing/copying their T when
    they want to execute an operation.
    There may be a cleaner way to do this, but for now, as a start it
    suffices.

--- lina/src/vector/mul.rs
+++ lina/src/vector/mul.rs
@@ -1,11 +1,13 @@
 use super::{Vector, v};
 
+// * operator for Vector<T> * T
 impl<ValueType, const LENGTH: usize> std::ops::Mul<ValueType> for Vector<ValueType, LENGTH>
 where
     ValueType: std::ops::Mul<Output = ValueType> + Default + Copy,
 {
     type Output = Vector<ValueType, LENGTH>;
 
+    /// Performs the `Vector<T> * T` operation.
     fn mul(self, rhs: ValueType) -> Self::Output {
         let mut result = v![ValueType::default(); LENGTH];
         self.data.iter().enumerate().for_each(|(i, value)| {
@@ -15,9 +17,53 @@ where
     }
 }
 
-// Unfortunately we can't implement the other direction where a
-// a number is on the LHS and a Vector on the RHS, because of the Orphan
-// rule. In this case both the LHS and the std::ops::Mul are foreign entities.
+impl<ValueType, const LENGTH: usize> std::ops::Mul<ValueType> for &Vector<ValueType, LENGTH>
+where
+    ValueType: std::ops::Mul<Output = ValueType> + Default + Copy,
+{
+    type Output = Vector<ValueType, LENGTH>;
+
+    /// Performs the `&Vector<T> * T` operation.
+    fn mul(self, rhs: ValueType) -> Self::Output {
+        let mut result = v![ValueType::default(); LENGTH];
+        self.data.iter().enumerate().for_each(|(i, value)| {
+            result.data[i] = *value * rhs;
+        });
+        result
+    }
+}
+
+impl<ValueType, const LENGTH: usize> std::ops::Mul<&ValueType> for Vector<ValueType, LENGTH>
+where
+    ValueType: std::ops::Mul<Output = ValueType> + Default + Copy,
+{
+    type Output = Vector<ValueType, LENGTH>;
+
+    /// Performs the `Vector<T> * &T` operation.
+    fn mul(self, rhs: &ValueType) -> Self::Output {
+        let mut result = v![ValueType::default(); LENGTH];
+        self.data.iter().enumerate().for_each(|(i, value)| {
+            result.data[i] = *value * *rhs;
+        });
+        result
+    }
+}
+
+impl<ValueType, const LENGTH: usize> std::ops::Mul<&ValueType> for &Vector<ValueType, LENGTH>
+where
+    ValueType: std::ops::Mul<Output = ValueType> + Default + Copy,
+{
+    type Output = Vector<ValueType, LENGTH>;
+
+    /// Performs the `&Vector<T> * &T` operation.
+    fn mul(self, rhs: &ValueType) -> Self::Output {
+        let mut result = v![ValueType::default(); LENGTH];
+        self.data.iter().enumerate().for_each(|(i, value)| {
+            result.data[i] = *value * *rhs;
+        });
+        result
+    }
+}
 
 #[cfg(test)]
 mod tests {
@@ -27,6 +73,29 @@ mod tests {
     fn scalar_mul() {
         let v = v![1, 2, 3];
         let result = v * 3;
+        assert_eq!(result.as_slice(), [3, 6, 9]);
+    }
+
+    #[test]
+    fn scalar_mul_by_ref() {
+        let v = v![1, 2, 3];
+        let result = &v * 3;
+        assert_eq!(result.as_slice(), [3, 6, 9]);
+    }
+
+    #[test]
+    fn scalar_ref_mul() {
+        let k: &i32 = &3;
+        let v = v![1, 2, 3];
+        let result = v * k;
+        assert_eq!(result.as_slice(), [3, 6, 9]);
+    }
+
+    #[test]
+    fn scalar_ref_mul_by_ref() {
+        let k: &i32 = &3;
+        let v = v![1, 2, 3];
+        let result = &v * k;
         assert_eq!(result.as_slice(), [3, 6, 9]);
     }
 }
