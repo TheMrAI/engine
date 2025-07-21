b0723228f5e17ba7d465b2da75c66b7b67c107ac
Istvan Davorin Acs <mr.istvan.acs@gmail.com>
2025-07-21 22:08:05 +0200

    Add support for LHS based scalar Vector multiplication
    We can only implement this for the built in types and any
    other must be provided by the users, should they require them.
    The orphan rule does not allow us to implement a foreign trait for
    foreign types.
    Supported formats:
    T * Vector<T>
    T * &Vector<T>
    &T * Vector<T>
    &T * &Vector<T>

--- lina/src/vector/mul.rs
+++ lina/src/vector/mul.rs
@@ -65,6 +65,66 @@ where
     }
 }
 
+// Implement the LHS scalar multiplication operators for built in types.
+// For custom types the user must provide the implementation given the Orphan rule.
+
+macro_rules! lhs_scalar_mul_impl {
+    ($($T: ty),* $(,)*) => {$(
+        // * operator for Vector<T> * T
+        impl<const LENGTH: usize> std::ops::Mul<Vector<$T, LENGTH>> for $T
+        where
+            $T: std::ops::Mul<Output = $T> + Default + Copy,
+        {
+            type Output = Vector<$T, LENGTH>;
+
+            /// Performs the `T * Vector<T>` operation.
+            fn mul(self, rhs: Vector<$T, LENGTH>) -> Self::Output {
+                rhs * self
+            }
+        }
+
+        impl<const LENGTH: usize> std::ops::Mul<&Vector<$T, LENGTH>> for $T
+        where
+            $T: std::ops::Mul<Output = $T> + Default + Copy,
+        {
+            type Output = Vector<$T, LENGTH>;
+
+            /// Performs the `T * &Vector<T>` operation.
+            fn mul(self, rhs: &Vector<$T, LENGTH>) -> Self::Output {
+                rhs * self
+            }
+        }
+
+        impl<const LENGTH: usize> std::ops::Mul<Vector<$T, LENGTH>> for &$T
+        where
+            $T: std::ops::Mul<Output = $T> + Default + Copy,
+        {
+            type Output = Vector<$T, LENGTH>;
+
+            /// Performs the `&T * Vector<T>` operation.
+            fn mul(self, rhs: Vector<$T, LENGTH>) -> Self::Output {
+                rhs * self
+            }
+        }
+
+        impl<const LENGTH: usize> std::ops::Mul<&Vector<$T, LENGTH>> for &$T
+        where
+            $T: std::ops::Mul<Output = $T> + Default + Copy,
+        {
+            type Output = Vector<$T, LENGTH>;
+
+            /// Performs the `&T * &Vector<T>` operation.
+            fn mul(self, rhs: &Vector<$T, LENGTH>) -> Self::Output {
+                rhs * self
+            }
+        }
+    )*};
+}
+
+lhs_scalar_mul_impl!(
+    u8, u16, u32, u64, u128, usize, i8, i16, i32, i64, i128, isize, f32, f64
+);
+
 #[cfg(test)]
 mod tests {
     use crate::v;
@@ -96,6 +156,36 @@ mod tests {
         let k: &i32 = &3;
         let v = v![1, 2, 3];
         let result = &v * k;
+        assert_eq!(result.as_slice(), [3, 6, 9]);
+    }
+
+    #[test]
+    fn scalar_mul_lhs() {
+        let v = v![1, 2, 3];
+        let result = 3i32 * v;
+        assert_eq!(result.as_slice(), [3, 6, 9]);
+    }
+
+    #[test]
+    fn scalar_mul_by_ref_lhs() {
+        let v = v![1, 2, 3];
+        let result = 3i32 * &v;
+        assert_eq!(result.as_slice(), [3, 6, 9]);
+    }
+
+    #[test]
+    fn scalar_ref_mul_lhs() {
+        let k: &i32 = &3;
+        let v = v![1, 2, 3];
+        let result = k * v;
+        assert_eq!(result.as_slice(), [3, 6, 9]);
+    }
+
+    #[test]
+    fn scalar_ref_mul_by_ref_lhs() {
+        let k: &i32 = &3;
+        let v = v![1, 2, 3];
+        let result = k * &v;
         assert_eq!(result.as_slice(), [3, 6, 9]);
     }
 }
