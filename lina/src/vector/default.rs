d76b45bd6fddbd9db63edf8370b2e2bd40e1111e
Istvan Davorin Acs <mr.istvan.acs@gmail.com>
2025-07-27 22:07:01 +0200

    Remove float test from lina::Vector Default implementation
    It was more than likely incorrect.

--- lina/src/vector/default.rs
+++ lina/src/vector/default.rs
@@ -19,10 +19,5 @@ mod tests {
     fn default() {
         let v_int = Vector::<i32, 5>::default();
         assert_eq!(v_int.as_slice(), &[0, 0, 0, 0, 0]);
-
-        let v_float = Vector::<f32, 5>::default();
-        // Comparing with zero value, should always work even with floats
-        // as that is a cleanly representable value.
-        assert_eq!(v_float.as_slice(), &[0.0, 0.0, 0.0, 0.0, 0.0]);
     }
 }
