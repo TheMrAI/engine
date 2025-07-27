d8cd9923e4a3b258a25322a280ae0ff2cd8b834a
Istvan Davorin Acs <mr.istvan.acs@gmail.com>
2025-07-27 22:26:25 +0200

    Refactor std::ops::Mul/MulAssign for lina::Vector
    After finally managign to use uninitialized memory for the std::ops::Div
    implementation it was clear that there was some misunderstanding
    regarding Copy and referencedness.
    For some reason I believed that
    Vector<T> * &T
    &Vector<T> * &T
    etc implementations would be helpful. But in reality they serve only
    to obfuscate the underlying operations.
    &Vector<T> * &T looks like we are taking the components by reference
    and not copying any of them, but this isn't the case in reality.
    When we get down to the implementation itself we realize that
    at some point std::ops::Mul<T> for T has to be implemented.
    Which takes by value, so it will destroy the input at the bottom level
    either way.
    
    Furthermore since uninitialized memory requires Copy to be implemented
    we might as well require it for the lina::Vector class itself. With
    this it was really clear that the operators were implemented
    incorrectly.

--- lina/src/vector/mul_assign.rs
+++ lina/src/vector/mul_assign.rs
@@ -3,25 +3,13 @@ use std::ops::MulAssign;
 
 impl<ValueType, const LENGTH: usize> MulAssign<ValueType> for Vector<ValueType, LENGTH>
 where
-    for<'a> &'a Vector<ValueType, LENGTH>:
-        std::ops::Mul<ValueType, Output = Vector<ValueType, LENGTH>>,
+    ValueType: std::ops::MulAssign<ValueType> + Copy,
 {
-    /// Implement the Vector<T> * T operation.
+    /// Perform the `Vector<T> * T` operation
     fn mul_assign(&mut self, rhs: ValueType) {
-        let result = &*self * rhs;
-        *self = result;
-    }
-}
-
-impl<ValueType, const LENGTH: usize> MulAssign<&ValueType> for Vector<ValueType, LENGTH>
-where
-    for<'a, 'b> &'a Vector<ValueType, LENGTH>:
-        std::ops::Mul<&'b ValueType, Output = Vector<ValueType, LENGTH>>,
-{
-    /// Implement the Vector<T> * &T operation.
-    fn mul_assign(&mut self, rhs: &ValueType) {
-        let result = &*self * rhs;
-        *self = result;
+        for elem in self.data.iter_mut() {
+            *elem *= rhs;
+        }
     }
 }
 
@@ -33,14 +21,6 @@ mod tests {
     fn scalar_mul() {
         let mut v = v![1, 2, 3];
         v *= 3;
-        assert_eq!(v.as_slice(), [3, 6, 9]);
-    }
-
-    #[test]
-    fn scalar_ref_mul() {
-        let s = &3;
-        let mut v = v![1, 2, 3];
-        v *= s;
         assert_eq!(v.as_slice(), [3, 6, 9]);
     }
 }
