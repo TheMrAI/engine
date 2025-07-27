217c0bc56dd762e86d6b33e3f8dc91e865dd2a21
Istvan Davorin Acs <mr.istvan.acs@gmail.com>
2025-07-27 21:44:05 +0200

    Add documentation to vector.rs
    During this process, a few mistakes have been identified.
    `from_value` does not require the `Default` trait to be implemented.
    `from` from array reference was frivolous.

--- lina/src/vector/vector.rs
+++ lina/src/vector/vector.rs
@@ -1,27 +1,52 @@
-#[derive(Debug, PartialEq, Clone)]
+/// General [Vector] structure
+///
+/// A simple `vector` type, which does not attempt
+/// to make assumptions about its purpose. In fact
+/// internally it is no more than a simple `array`.
+///
+/// ## Requirements
+///
+/// `ValueType` must implement [std::marker::Copy] trait.
+/// This is to support standard operator implementations
+/// and uninitialized memory allocation.
+/// Otherwise [Vector] does not impose other requirements
+/// only those that are necessary for each trait implementation.
+#[derive(Debug, PartialEq, Clone, Copy)]
 pub struct Vector<ValueType, const LENGTH: usize> {
     pub(crate) data: [ValueType; LENGTH],
 }
-
-// impl<ValueType, const LENGTH: usize> Clone for Vector<ValueType, LENGTH>
-// where
-//     ValueType: Clone,
-// {
-//     fn clone(&self) -> Self {
-//         Self {
-//             data: self.data.clone(),
-//         }
-//     }
-// }
 
 impl<ValueType, const LENGTH: usize> Vector<ValueType, LENGTH>
 where
     ValueType: Default + Copy,
 {
+    /// Create a new [Vector] filled with [Default](std::default::Default) of `ValueType`.
+    ///
+    /// Example
+    /// ```
+    /// # use lina::vector::Vector;
+    /// let v1 : Vector<i32, 3> = Vector::new();
+    /// // or
+    /// let v2 = Vector::<i32, 3>::new();
+    /// ```
     pub fn new() -> Self {
         Self::default()
     }
+}
 
+impl<ValueType, const LENGTH: usize> Vector<ValueType, LENGTH>
+where
+    ValueType: Copy,
+{
+    /// Create a new [Vector] filled with `default_value`.
+    ///
+    /// Example
+    /// ```
+    /// # use lina::vector::Vector;
+    /// let v1 : Vector<i32, 3> = Vector::from_value(3);
+    /// // or
+    /// let v2 = Vector::<i32, 3>::from_value(3);
+    /// ```
     pub fn from_value(default_value: ValueType) -> Self {
         Self {
             data: [default_value; LENGTH],
@@ -30,10 +55,12 @@ where
 }
 
 impl<ValueType, const LENGTH: usize> Vector<ValueType, LENGTH> {
+    /// Create a slice into the internal data
     pub fn as_slice(&self) -> &[ValueType] {
         &self.data
     }
 
+    /// Construct a [Vector] from the given slice
     pub fn from_array(values: [ValueType; LENGTH]) -> Self {
         Self { data: values }
     }
@@ -49,17 +76,21 @@ where
 }
 
 impl<ValueType, const LENGTH: usize> From<[ValueType; LENGTH]> for Vector<ValueType, LENGTH> {
+    /// Create a [Vector] from an array
+    ///
+    /// ```
+    /// # use lina::vector::Vector;
+    /// let array = [1, 2, 3];
+    /// let v = Vector::from(array);
+    /// ```
+    /// or using the generated [Into] trait.
+    /// ```
+    /// # use lina::vector::Vector;
+    /// let array = [1,2,3];
+    /// let v : Vector<i32, 3> = array.into();
+    /// ```
     fn from(values: [ValueType; LENGTH]) -> Self {
         Vector::from_array(values)
-    }
-}
-
-impl<ValueType, const LENGTH: usize> From<&[ValueType; LENGTH]> for Vector<ValueType, LENGTH>
-where
-    ValueType: Clone,
-{
-    fn from(values: &[ValueType; LENGTH]) -> Self {
-        Vector::from_array(values.clone())
     }
 }
 
@@ -85,5 +116,12 @@ mod tests {
     fn macro_init_with_set_values() {
         let vector = v![1, 2, 3, 4];
         assert_eq!(vector.as_slice(), &[1, 2, 3, 4]);
+    }
+
+    #[test]
+    fn eq() {
+        let v1 = v![1, 2, 3];
+        let v2 = v![1, 2, 3];
+        assert!(v1 == v2);
     }
 }
