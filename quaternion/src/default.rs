92aa3370a07d058d7a4fafbda82c4415b1a85a4f
Istvan Davorin Acs <mr.istvan.acs@gmail.com>
2025-11-16 14:29:26 +0100

    Update Camera to use quaternions for orientation
    Now a proper FPS movement is supported, hoefully making navigating and
    inspecting the scenes much easier.

--- quaternion/src/default.rs
+++ quaternion/src/default.rs
@@ -5,7 +5,7 @@ use crate::Quaternion;
 impl<ValueType> Default for Quaternion<ValueType>
 where
     ValueType: Default + Copy,
-    ValueType: std::convert::From<i32>,
+    ValueType: std::convert::From<i8>,
 {
     fn default() -> Self {
         Self {
@@ -23,7 +23,7 @@ mod tests {
 
     #[test]
     fn default() {
-        let q = Quaternion::<i32>::default();
+        let q = Quaternion::default();
         assert_eq!(q.scalar(), 1);
         assert_eq!(q.vector(), Vector::from_value(0));
     }
