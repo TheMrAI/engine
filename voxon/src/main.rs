016f65dd865f6feb2f9ec5c2f2c30bf1386a9838
Istvan Davorin Acs <mr.istvan.acs@gmail.com>
2025-12-30 16:47:26 +0100

    Add support for multiple key presses
    Now multiple keyboard presses are properly registered by the application
    and the speed of change is scalled according to the framerate and
    the defined base speed.
    This provides a nice and smooth navigation experience in contrast
    to the previous jerky one (didn't even notice it really, was
    so focused on the rendering aspects).
    
    Also added support for the Shift key, which in will tripple the move
    speed when held. Not sure how much would I use it, but Godot
    does something similar, so it is probabbly a good idea.

--- voxon/src/main.rs
+++ voxon/src/main.rs
@@ -15,7 +15,9 @@ struct App {
     app: Option<InnerApp>,
     focused: bool,
     navigating: bool,
-    speed_ms: f32, // speed in m/s
+    speed: f32, // speed in m/s
+    // stores for each key if it is currently being pressed/held or not
+    key_state: std::collections::BTreeMap<winit::keyboard::KeyCode, bool>,
 }
 
 impl Default for App {
@@ -24,7 +26,8 @@ impl Default for App {
             app: None,
             focused: false,
             navigating: false,
-            speed_ms: 1.0,
+            speed: 1.0,
+            key_state: Default::default(),
         }
     }
 }
@@ -54,10 +57,79 @@ impl ApplicationHandler for App {
                 // this event rather than in AboutToWait, since rendering in here allows
                 // the program to gracefully handle redraws requested by the OS.
 
+                // Before redraw, apply all navigation changes.
+                let key_left_shift = self
+                    .key_state
+                    .get(&winit::keyboard::KeyCode::ShiftLeft)
+                    .cloned()
+                    .unwrap_or(false);
+                let key_right_shift = self
+                    .key_state
+                    .get(&winit::keyboard::KeyCode::ShiftRight)
+                    .cloned()
+                    .unwrap_or(false);
+                let key_w = self
+                    .key_state
+                    .get(&winit::keyboard::KeyCode::KeyW)
+                    .cloned()
+                    .unwrap_or(false);
+                let key_s = self
+                    .key_state
+                    .get(&winit::keyboard::KeyCode::KeyS)
+                    .cloned()
+                    .unwrap_or(false);
+                let key_d = self
+                    .key_state
+                    .get(&winit::keyboard::KeyCode::KeyD)
+                    .cloned()
+                    .unwrap_or(false);
+                let key_a = self
+                    .key_state
+                    .get(&winit::keyboard::KeyCode::KeyA)
+                    .cloned()
+                    .unwrap_or(false);
+                let key_e = self
+                    .key_state
+                    .get(&winit::keyboard::KeyCode::KeyE)
+                    .cloned()
+                    .unwrap_or(false);
+                let key_q = self
+                    .key_state
+                    .get(&winit::keyboard::KeyCode::KeyQ)
+                    .cloned()
+                    .unwrap_or(false);
+
                 // Draw.
                 if let Some(app) = self.app.as_mut() {
                     let current_time = std::time::Instant::now();
                     let delta_t = current_time.duration_since(app.prev_render_time);
+
+                    let elapsed_s = delta_t.as_secs_f32();
+                    let speed = if key_left_shift || key_right_shift {
+                        3.0 * self.speed * elapsed_s
+                    } else {
+                        1.0 * self.speed * elapsed_s
+                    };
+
+                    if key_w {
+                        app.camera.move_on_look_at_vector(speed);
+                    };
+                    if key_s {
+                        app.camera.move_on_look_at_vector(-speed);
+                    };
+                    if key_d {
+                        app.camera.move_on_right_vector(speed);
+                    };
+                    if key_a {
+                        app.camera.move_on_right_vector(-speed);
+                    };
+                    if key_e {
+                        app.camera.move_on_up_vector(speed);
+                    };
+                    if key_q {
+                        app.camera.move_on_up_vector(-speed);
+                    }
+
                     app.gpu
                         .render(&app.camera, delta_t, &mut app.delta_t_for_cube);
                     // for continuos rendering
@@ -68,7 +140,17 @@ impl ApplicationHandler for App {
                 }
                 // else nothing to do yet
             }
-            WindowEvent::Focused(focus_state) => self.focused = focus_state,
+            WindowEvent::Focused(focused) => {
+                if !focused {
+                    // If focus is lost from the application
+                    // we simply clear all keys. Resetting the state.
+                    // Otherwise the user could click away while holding a
+                    // key, let go of the key, but that will no longer be
+                    // registered by the application.
+                    self.key_state.clear();
+                }
+                self.focused = focused
+            }
             WindowEvent::CursorEntered { device_id: _ } => {}
             WindowEvent::CursorLeft { device_id: _ } => {}
             WindowEvent::Resized(inner_resolution) => {
@@ -99,33 +181,17 @@ impl ApplicationHandler for App {
         #[allow(clippy::single_match)]
         match event {
             DeviceEvent::Key(key_event) => {
-                if let Some(app) = self.app.as_mut() {
-                    if !key_event.state.is_pressed() {
-                        return;
-                    }
-                    // camera navigation controls for the engine
-                    if self.focused && self.navigating {
-                        match key_event.physical_key {
-                            PhysicalKey::Code(winit::keyboard::KeyCode::KeyW) => {
-                                app.camera.move_on_look_at_vector(self.speed_ms);
-                            }
-                            PhysicalKey::Code(winit::keyboard::KeyCode::KeyS) => {
-                                app.camera.move_on_look_at_vector(-self.speed_ms);
-                            }
-                            PhysicalKey::Code(winit::keyboard::KeyCode::KeyD) => {
-                                app.camera.move_on_right_vector(self.speed_ms);
-                            }
-                            PhysicalKey::Code(winit::keyboard::KeyCode::KeyA) => {
-                                app.camera.move_on_right_vector(-self.speed_ms);
-                            }
-                            PhysicalKey::Code(winit::keyboard::KeyCode::KeyE) => {
-                                app.camera.move_on_up_vector(self.speed_ms);
-                            }
-                            PhysicalKey::Code(winit::keyboard::KeyCode::KeyQ) => {
-                                app.camera.move_on_up_vector(-self.speed_ms);
-                            }
-                            _ => (),
+                // camera navigation controls for the engine
+                if self.focused && self.navigating {
+                    match key_event.physical_key {
+                        PhysicalKey::Code(key_code) => {
+                            let is_pressed = key_event.state == ElementState::Pressed;
+                            self.key_state
+                                .entry(key_code)
+                                .and_modify(|entry| *entry = is_pressed)
+                                .or_insert(is_pressed);
                         }
+                        _ => {}
                     }
                 }
             }
@@ -149,8 +215,8 @@ impl ApplicationHandler for App {
                             // This way within the range os 0.1 - 30 the user
                             // gets finer control on the lower ends and coarser on the
                             // higher ends.
-                            self.speed_ms += dy * ((self.speed_ms + 1.0).log2() / 2.0);
-                            self.speed_ms = self.speed_ms.clamp(0.1, 30.0);
+                            self.speed += dy * ((self.speed + 1.0).log2() / 2.0);
+                            self.speed = self.speed.clamp(0.1, 30.0);
                         }
                         MouseScrollDelta::PixelDelta(_) => {}
                     }
