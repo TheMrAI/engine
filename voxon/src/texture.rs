e1ae8b152a024f3346436d4492b41fd0b9d9a536
MrAI <mr.istvan.acs@gmail.com>
2026-03-29 16:59:42 +0200

    Enable the loading of cubemap textures
    Now the upload texture function can handle both simple 2D and cubemap
    textures as well.
    It is a bit painful that textures are compiled into the binary, but it
    is acceptible for now.
    The biggest issue right now is actually making it functional, not
    pretty. Moreover it is rather early to be able to tell what
    abstractions are even worth it.

--- voxon/src/texture.rs
+++ voxon/src/texture.rs
@@ -12,7 +12,7 @@ pub fn load_texture_plane(device: &wgpu::Device, queue: &wgpu::Queue) -> wgpu::T
         depth_or_array_layers: 1,
     };
 
-    let texture = upload_texture(device, queue, dimensions, bytes, true);
+    let texture = upload_texture(device, queue, dimensions, &[bytes], true);
 
     texture.create_view(&wgpu::wgt::TextureViewDescriptor::default())
 }
@@ -31,9 +31,86 @@ pub fn load_texture_cube(device: &wgpu::Device, queue: &wgpu::Queue) -> wgpu::Te
         depth_or_array_layers: 1,
     };
 
-    let texture = upload_texture(device, queue, dimensions, bytes, true);
+    let texture = upload_texture(device, queue, dimensions, &[bytes], true);
 
     texture.create_view(&wgpu::wgt::TextureViewDescriptor::default())
+}
+
+pub fn load_cubemap_textures(device: &wgpu::Device, queue: &wgpu::Queue) -> wgpu::TextureView {
+    // This is nasty, as the textures are compiled into the binary, but for now it is okay.
+    // px
+    let image_data = include_bytes!("../resources/textures/skybox/sky_cube_px.png");
+    let png_decoder = png::Decoder::new(std::io::Cursor::new(image_data));
+    let mut reader = png_decoder.read_info().unwrap();
+    let mut buf = vec![0; reader.output_buffer_size().unwrap()];
+    let frame_info = reader.next_frame(&mut buf).unwrap();
+    let px_bytes = &buf[..frame_info.buffer_size()];
+    // nx
+    let image_data = include_bytes!("../resources/textures/skybox/sky_cube_nx.png");
+    let png_decoder = png::Decoder::new(std::io::Cursor::new(image_data));
+    let mut reader = png_decoder.read_info().unwrap();
+    let mut buf = vec![0; reader.output_buffer_size().unwrap()];
+    let frame_info = reader.next_frame(&mut buf).unwrap();
+    let nx_bytes = &buf[..frame_info.buffer_size()];
+    // py
+    let image_data = include_bytes!("../resources/textures/skybox/sky_cube_py.png");
+    let png_decoder = png::Decoder::new(std::io::Cursor::new(image_data));
+    let mut reader = png_decoder.read_info().unwrap();
+    let mut buf = vec![0; reader.output_buffer_size().unwrap()];
+    let frame_info = reader.next_frame(&mut buf).unwrap();
+    let py_bytes = &buf[..frame_info.buffer_size()];
+    // ny
+    let image_data = include_bytes!("../resources/textures/skybox/sky_cube_ny.png");
+    let png_decoder = png::Decoder::new(std::io::Cursor::new(image_data));
+    let mut reader = png_decoder.read_info().unwrap();
+    let mut buf = vec![0; reader.output_buffer_size().unwrap()];
+    let frame_info = reader.next_frame(&mut buf).unwrap();
+    let ny_bytes = &buf[..frame_info.buffer_size()];
+    // pz
+    let image_data = include_bytes!("../resources/textures/skybox/sky_cube_pz.png");
+    let png_decoder = png::Decoder::new(std::io::Cursor::new(image_data));
+    let mut reader = png_decoder.read_info().unwrap();
+    let mut buf = vec![0; reader.output_buffer_size().unwrap()];
+    let frame_info = reader.next_frame(&mut buf).unwrap();
+    let pz_bytes = &buf[..frame_info.buffer_size()];
+    // nz
+    let image_data = include_bytes!("../resources/textures/skybox/sky_cube_nz.png");
+    let png_decoder = png::Decoder::new(std::io::Cursor::new(image_data));
+    let mut reader = png_decoder.read_info().unwrap();
+    let mut buf = vec![0; reader.output_buffer_size().unwrap()];
+    let frame_info = reader.next_frame(&mut buf).unwrap();
+    let nz_bytes = &buf[..frame_info.buffer_size()];
+
+    // The order of the buffers matters.
+    // See: https://gpuweb.github.io/gpuweb/#dom-gputextureviewdimension-cube
+    let cubemap_buffers = &[px_bytes, nx_bytes, py_bytes, ny_bytes, pz_bytes, nz_bytes];
+
+    // This part is confusing.
+    // A dimension struct defines that we are working with a texture that has 3 dimensions.
+    // Do not be confused though. It seems that for a cubemap **..or_array_layers** part is
+    // the important one. In case, the third value does not represent **depth**, then we
+    // should not think of a texture as 3 dimensional. It merely has multiple layers.
+    let dimensions = wgpu::Extent3d {
+        width: frame_info.width,
+        height: frame_info.height,
+        depth_or_array_layers: 6,
+    };
+
+    let texture = upload_texture(device, queue, dimensions, cubemap_buffers, true);
+
+    // A texture view is created, which says the underlying texture satisfies
+    // the requirements of being a cube(map).
+    texture.create_view(&wgpu::wgt::TextureViewDescriptor {
+        label: Some("cube_view"),
+        format: None,
+        dimension: Some(wgpu::TextureViewDimension::Cube),
+        usage: Some(wgpu::TextureUsages::TEXTURE_BINDING),
+        aspect: wgpu::TextureAspect::All,
+        base_mip_level: 0,
+        mip_level_count: None, // include all mipmap levels
+        base_array_layer: 0,
+        array_layer_count: Some(6),
+    })
 }
 
 // Upload a 2D texture to the GPU and generate mipmaps if requested.
@@ -45,9 +122,12 @@ pub fn upload_texture(
     device: &wgpu::Device,
     queue: &wgpu::Queue,
     dimensions: wgpu::Extent3d,
-    texture_buffers: &[u8],
+    texture_buffers: &[&[u8]],
     generate_mips: bool,
 ) -> wgpu::Texture {
+    debug_assert!(dimensions.depth_or_array_layers > 0);
+    debug_assert!(texture_buffers.len() == dimensions.depth_or_array_layers as usize);
+
     let mip_level_count = {
         if generate_mips {
             std::cmp::max(dimensions.width.ilog2(), dimensions.height.ilog2())
@@ -74,16 +154,41 @@ pub fn upload_texture(
         depth_or_array_layers: 1,
         ..dimensions
     };
-    queue.write_texture(
-        texture.as_image_copy(),
-        texture_buffers,
-        wgpu::TexelCopyBufferLayout {
-            offset: 0,
-            bytes_per_row: Some(dimensions.width * 4),
-            rows_per_image: None,
-        },
-        single_texture_extent,
-    );
+
+    if dimensions.depth_or_array_layers == 1 {
+        queue.write_texture(
+            texture.as_image_copy(),
+            texture_buffers[0],
+            wgpu::TexelCopyBufferLayout {
+                offset: 0,
+                bytes_per_row: Some(dimensions.width * 4),
+                rows_per_image: None,
+            },
+            single_texture_extent,
+        );
+    } else {
+        for layer in 0..dimensions.depth_or_array_layers {
+            queue.write_texture(
+                wgpu::TexelCopyTextureInfo {
+                    texture: &texture,
+                    mip_level: 0,
+                    origin: wgpu::Origin3d {
+                        x: 0,
+                        y: 0,
+                        z: layer,
+                    },
+                    aspect: wgpu::TextureAspect::All,
+                },
+                texture_buffers[layer as usize],
+                wgpu::TexelCopyBufferLayout {
+                    offset: 0,
+                    bytes_per_row: Some(dimensions.width * 4),
+                    rows_per_image: None,
+                },
+                single_texture_extent,
+            );
+        }
+    }
 
     if generate_mips {
         let mut encoder = device.create_command_encoder(&wgpu::CommandEncoderDescriptor {
@@ -147,59 +252,73 @@ pub fn generate_mipmap(
         ..Default::default()
     });
 
-    let views = (0..mip_count)
-        .map(|mip_level| {
-            texture.create_view(&wgpu::TextureViewDescriptor {
-                label: Some("mip_level"),
-                format: None,
-                dimension: None,
-                usage: Some(
-                    wgpu::TextureUsages::TEXTURE_BINDING | wgpu::TextureUsages::RENDER_ATTACHMENT,
-                ),
-                aspect: wgpu::TextureAspect::All,
-                base_mip_level: mip_level,
-                mip_level_count: Some(1),
-                base_array_layer: 0,
-                array_layer_count: None,
-            })
+    let layer_views = (0..texture.depth_or_array_layers())
+        .map(|layer| {
+            let array_layer_count = if texture.depth_or_array_layers() == 1 {
+                None
+            } else {
+                Some(1)
+            };
+
+            // views
+            (0..mip_count)
+                .map(|mip_level| {
+                    texture.create_view(&wgpu::TextureViewDescriptor {
+                        label: Some("mip_level"),
+                        format: None,
+                        dimension: Some(wgpu::TextureViewDimension::D2),
+                        usage: Some(
+                            wgpu::TextureUsages::TEXTURE_BINDING
+                                | wgpu::TextureUsages::RENDER_ATTACHMENT,
+                        ),
+                        aspect: wgpu::TextureAspect::All,
+                        base_mip_level: mip_level,
+                        mip_level_count: Some(1),
+                        base_array_layer: layer,
+                        array_layer_count,
+                    })
+                })
+                .collect::<Vec<_>>()
         })
         .collect::<Vec<_>>();
 
     for mip_level in 1..mip_count as usize {
-        let bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
-            label: Some("mipmap_bind_group"),
-            layout: &bind_group_layout,
-            entries: &[
-                wgpu::BindGroupEntry {
-                    binding: 0,
-                    resource: wgpu::BindingResource::Sampler(&sampler),
-                },
-                wgpu::BindGroupEntry {
-                    binding: 1,
-                    resource: wgpu::BindingResource::TextureView(&views[mip_level - 1]),
-                },
-            ],
-        });
+        for views in &layer_views {
+            let bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
+                label: Some("mipmap_bind_group"),
+                layout: &bind_group_layout,
+                entries: &[
+                    wgpu::BindGroupEntry {
+                        binding: 0,
+                        resource: wgpu::BindingResource::Sampler(&sampler),
+                    },
+                    wgpu::BindGroupEntry {
+                        binding: 1,
+                        resource: wgpu::BindingResource::TextureView(&views[mip_level - 1]),
+                    },
+                ],
+            });
 
-        let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
-            label: Some("mipmap_render_pass"),
-            color_attachments: &[Some(wgpu::RenderPassColorAttachment {
-                view: &views[mip_level],
-                depth_slice: None,
-                resolve_target: None,
-                ops: wgpu::Operations {
-                    load: wgpu::LoadOp::Clear(wgpu::Color::BLACK),
-                    store: wgpu::StoreOp::Store,
-                },
-            })],
-            depth_stencil_attachment: None,
-            timestamp_writes: None,
-            occlusion_query_set: None,
-            multiview_mask: None,
-        });
+            let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                label: Some("mipmap_render_pass"),
+                color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+                    view: &views[mip_level],
+                    depth_slice: None,
+                    resolve_target: None,
+                    ops: wgpu::Operations {
+                        load: wgpu::LoadOp::Clear(wgpu::Color::BLACK),
+                        store: wgpu::StoreOp::Store,
+                    },
+                })],
+                depth_stencil_attachment: None,
+                timestamp_writes: None,
+                occlusion_query_set: None,
+                multiview_mask: None,
+            });
 
-        render_pass.set_pipeline(&pipeline);
-        render_pass.set_bind_group(0, &bind_group, &[]);
-        render_pass.draw(0..3, 0..1);
+            render_pass.set_pipeline(&pipeline);
+            render_pass.set_bind_group(0, &bind_group, &[]);
+            render_pass.draw(0..3, 0..1);
+        }
     }
 }
