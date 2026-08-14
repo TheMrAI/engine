2c034bb2617cbdfc99f7b8386abd9bdd0e132b50
MrAI <mr.istvan.acs@gmail.com>
2026-08-14 14:10:56 +0200

    Added test for adding components to the World
    It turns out that DynamicSignature may have been ill conceived.
    An Entity is nothing but a collection of ordered component ids, layouts
    , destructors and data. This collection must be kept in order
    according to component ids. Component ids cannot be known until runtime.
    Meaning that all component data must be ordered at runtime together.
    DynamicSignature is only capable ensuring that there are no duplicate
    ids and they are ordered, but it cannot match against other fields.
    
    Now we have a placeholder interface for adding an Entity to the world.
    A bit disturbing. We shall see in what way it can be made better, when
    the outline of the design takes shape. Pretty difficult to make any
    choices in this ECS model. Barely understand all the implications.

--- ecs/src/lib.rs
+++ ecs/src/lib.rs
@@ -1,11 +1,16 @@
-use std::{alloc, collections::HashMap, ptr};
+use std::{
+    alloc, any,
+    collections::HashMap,
+    ptr::{self, NonNull},
+};
 
 use cyclid::Cyclid;
 
-use crate::{archetype::Archetype, blob::Blob, signature::DynamicSignature};
+use crate::{archetype::Archetype, blob::Blob, registry::ComponentRegistry};
 
 pub mod archetype;
 mod blob;
+mod registry;
 pub mod signature;
 
 type ComponentDropFn = unsafe fn(ptr::NonNull<u8>) -> ();
@@ -13,6 +18,7 @@ type EntityId = u32;
 type ArchetypeId = usize;
 type ComponentId = u32;
 type ComponentLayouts = Vec<alloc::Layout>;
+type ComponentValues = Vec<NonNull<u8>>;
 type ArchetypeMap = HashMap<ArchetypeId, ArchetypeRecord>;
 
 #[derive(Debug, Default)]
@@ -29,9 +35,10 @@ struct Record {
 #[derive(Debug, Default)]
 pub struct World {
     entity_index: HashMap<EntityId, Record>,
-    archetype_index: HashMap<DynamicSignature, ArchetypeId>,
+    archetype_index: HashMap<Vec<ComponentId>, ArchetypeId>,
     component_index: HashMap<ComponentId, ArchetypeMap>,
     archetypes: Cyclid<Archetype>,
+    component_registry: ComponentRegistry,
 }
 
 impl World {
@@ -63,25 +70,40 @@ impl World {
         archetype.get_component(archetype_record.column, record.row)
     }
 
+    /// Add an entity to the world
+    ///
+    /// The function is a bit disturbing at the moment.
+    /// [EntityId] is passed in, and it must not collide with other
+    /// existing entities.
+    /// component_data is a tuple of vectors that must be properly ordered
+    /// in sync with [ComponentId].
+    /// The ordering is critically important as it uniquely identifies the
+    /// [Archetype].
+    /// TODO: need a design where this can't be messed up
     pub fn add_entity(
         &mut self,
         entity_id: EntityId,
-        entity_type: DynamicSignature,
-        component_layouts: ComponentLayouts,
+        component_data: (
+            Vec<ComponentId>,
+            ComponentLayouts,
+            Vec<Option<ComponentDropFn>>,
+            ComponentValues,
+        ),
     ) {
         let archetype_id = self
             .archetype_index
-            .entry(entity_type.clone())
+            .entry(component_data.0.clone())
             .or_insert_with(|| {
-                self.archetypes
-                    .insert(Archetype::with_capacity(5, component_layouts, vec![None]))
+                self.archetypes.insert(Archetype::with_capacity(
+                    5,
+                    component_data.1,
+                    component_data.2,
+                ))
             });
 
         let archetype = self.archetypes.get_mut(*archetype_id);
-        // testing hack
-        let mut val = 3u16;
-        let ptr = ptr::NonNull::new((&mut val as *mut u16).cast::<u8>()).unwrap();
-        let index = archetype.push(&[ptr]);
+
+        let index = archetype.push(&component_data.3);
         let record = Record {
             archetype_id: *archetype_id,
             row: index,
@@ -89,41 +111,102 @@ impl World {
 
         self.entity_index.insert(entity_id, record);
 
-        for component in entity_type.components() {
+        for (i, component) in component_data.0.iter().enumerate() {
             self.component_index
                 .entry(*component)
                 .and_modify(|archetype_map| {
-                    archetype_map.insert(0, ArchetypeRecord { column: 0 });
+                    archetype_map.insert(*archetype_id, ArchetypeRecord { column: i });
                 })
                 .or_insert({
                     let mut archetype_map = ArchetypeMap::new();
-                    archetype_map.insert(0, ArchetypeRecord { column: 0 });
+                    archetype_map.insert(*archetype_id, ArchetypeRecord { column: i });
                     archetype_map
                 });
         }
+    }
+
+    pub fn register_component<T: 'static>(&mut self) -> ComponentId {
+        self.component_registry
+            .get_or_insert_id(any::TypeId::of::<T>())
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use std::alloc;
+    use std::{
+        alloc,
+        ptr::{self, NonNull},
+    };
 
-    use crate::{World, signature::DynamicSignature};
+    use crate::{ComponentDropFn, ComponentId, World};
+
+    #[derive(Debug, PartialEq)]
+    struct ComponentA {
+        a: usize,
+    }
+
+    #[derive(Debug, PartialEq)]
+    struct ComponentB {
+        a: i32,
+        b: u8,
+        c: u64,
+    }
 
     #[test]
-    fn has() {
+    fn add_component() {
         let mut world = World::default();
-        world.add_entity(
-            0,
-            DynamicSignature::from_slice(&[1]),
-            vec![alloc::Layout::new::<u16>()],
-        );
-        println!("Has checking");
-        let expected = [false, true, false, false, false];
-        for i in 0..5 {
-            assert_eq!(world.has_component(0, i), expected[i as usize])
-        }
+        let mut component_a = ComponentA { a: 988888889 };
+        let mut component_b = ComponentB {
+            a: -33333,
+            b: 12,
+            c: 1234567890,
+        };
 
-        assert_eq!(unsafe { world.get_component(0, 1).cast::<u16>().read() }, 3);
+        let component_a_id = world.register_component::<ComponentA>();
+        let component_b_id = world.register_component::<ComponentB>();
+
+        // Component parts
+        let mut component_entries: Vec<(u32, alloc::Layout, Option<ComponentDropFn>, NonNull<u8>)> = vec![
+            (
+                component_a_id,
+                alloc::Layout::new::<ComponentA>(),
+                None,
+                unsafe { ptr::NonNull::new_unchecked(&mut component_a as *mut ComponentA).cast() },
+            ),
+            (
+                component_b_id,
+                alloc::Layout::new::<ComponentB>(),
+                None,
+                unsafe { ptr::NonNull::new_unchecked(&mut component_b as *mut ComponentB).cast() },
+            ),
+        ];
+        component_entries.sort_by(|lhs, rhs| lhs.0.cmp(&rhs.0));
+        let entity_data = {
+            let mut ids = Vec::<ComponentId>::new();
+            let mut layouts = Vec::<alloc::Layout>::new();
+            let mut drop_fns = Vec::<Option<ComponentDropFn>>::new();
+            let mut data = Vec::<NonNull<u8>>::new();
+
+            for (id, layout, drop_fn, value) in component_entries.into_iter() {
+                ids.push(id);
+                layouts.push(layout);
+                drop_fns.push(drop_fn);
+                data.push(value);
+            }
+
+            (ids, layouts, drop_fns, data)
+        };
+        world.add_entity(0, entity_data);
+
+        let stored_component_a = world.get_component(0, component_a_id).cast::<ComponentA>();
+        let stored_component_b = world.get_component(0, component_b_id).cast::<ComponentB>();
+
+        assert_eq!(unsafe { stored_component_a.as_ref().a }, component_a.a);
+        assert_eq!(unsafe { stored_component_b.as_ref().a }, component_b.a);
+        assert_eq!(unsafe { stored_component_b.as_ref().b }, component_b.b);
+        assert_eq!(unsafe { stored_component_b.as_ref().c }, component_b.c);
+
+        assert_eq!(&component_a, unsafe { stored_component_a.as_ref() });
+        assert_eq!(&component_b, unsafe { stored_component_b.as_ref() });
     }
 }
---
+++ ecs/src/registry.rs
@@ -0,0 +1,35 @@
+use std::{any, collections::HashMap};
+
+/// Registry for component types
+///
+/// The [any::TypeId] of the component type will be used
+/// to identify a type. This is then mapped onto a simple [u32] id.
+///
+/// The format or the value of the [any::TypeId] is unstable, but
+/// that is okay as we do not rely on it.
+/// [any::TypeId] only exists for types that
+/// adhere to `'static` lifetime. This means that there may be no
+/// non-static reference within the type itself.
+/// In terms of the logic of the ECS this should be okay, as the goal
+/// is to store data directly in continuous memory. It is directly
+/// detrimental to performance if additional indirections are present
+/// in the stored components.
+#[derive(Default, Debug)]
+pub struct ComponentRegistry {
+    components: HashMap<any::TypeId, u32>,
+    next_id: u32,
+}
+
+impl ComponentRegistry {
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    pub fn get_or_insert_id(&mut self, type_id: any::TypeId) -> u32 {
+        *self.components.entry(type_id).or_insert_with(|| {
+            let id = self.next_id;
+            self.next_id += 1;
+            id
+        })
+    }
+}
